bd1e685ff709d3481693050119fb599169da7328
AdminK <volvenkova.k@gmail.com>
2023-02-03 15:54:37 +0200

    add slider adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,40 @@ burger.onclick = function () {
     header.classList.toggle("menu_open");
 };
 
+// buttons change color language
 
-// carousel
+$("button.language__button").click(function(){
+    $("button.language__button").css("background-color", "transparent");
+  $(this).css("background-color", "#F0145A");
+});
+
+// buttons change color advantages
+
+$(document).ready(function(){
+    $('.advantages__titles li button').click(function(){
+      $('.advantages__titles li button').removeClass("advantages__button_active");
+      $(this).addClass("advantages__button_active");
+  });
+  });
+
+// appearance text
+$(document).on('click', '.advantages__button', function() {
+    var show = $(this).data('show');
+    $(show).removeClass("advantages__wrap-content_hide").siblings().addClass("advantages__wrap-content_hide");
+  });
+  
+
+// carousel homepage
+
+$(document).ready(function(){
+    $('.about-team__slider').slick({
+        dots: true,
+        arrows: false
+    });
+});
+
+
+// carousel about-page
 
 $(document).ready(function () {
     $('.slider').slick({
@@ -29,27 +61,3 @@ $(document).ready(function () {
     });
 });
 
-// buttons change color language
-
-$("button.language__button").click(function(){
-    $("button.language__button").css("background-color", "transparent");
-  $(this).css("background-color", "#F0145A");
-});
-
-// buttons change color advantages
-
-$(document).ready(function(){
-    $('.advantages__titles li button').click(function(){
-      $('.advantages__titles li button').removeClass("advantages__button_active");
-      $(this).addClass("advantages__button_active");
-  });
-  });
-
-
-
-// appearance text
-$(document).on('click', '.advantages__button', function() {
-    var show = $(this).data('show');
-    $(show).removeClass("advantages__wrap-content_hide").siblings().addClass("advantages__wrap-content_hide");
-  });
-  
