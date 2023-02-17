4ed6e87ab70c388921a6a90ed35b01765c8d4402
AdminK <volvenkova.k@gmail.com>
2023-02-17 22:27:58 +0200

    add publications page

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,6 @@ burger.onclick = function () {
     header.classList.toggle("menu_open");
 };
 
-
 $(document).ready(function () {
     // buttons change color language
     $('.header__lang li button').click(function () {
@@ -26,6 +25,47 @@ $(document).ready(function () {
         $('.advantages__titles li button').removeClass("advantages__button_active");
         $(this).addClass("advantages__button_active");
     });
+
+    // buttons change color switcher news
+    $('.publications__button').click(function () {
+        $('.publications__button').removeClass("publications__button_active");
+        $(this).addClass("publications__button_active");
+    });
+
+    // Content Switcher
+
+    $(".publications__articles").hide();
+
+    $("#switch-articles").click(function () {
+        $(".publications__articles").show();
+        $(".publications__news").hide();
+    });
+    $("#switch-news").click(function () {
+        $(".publications__articles").hide();
+        $(".publications__news").show();
+    });
+
+
+    // load-more news
+    $(".cards-news").slice(0, 6).show();
+    $("#more-news").on("click", function (e) {
+        e.preventDefault();
+        $(".cards-news:hidden").slice(0, 6).slideDown();
+        if ($(".cards-news:hidden").length == 0) {
+            $(".more-news").text("").addClass("more-news_no-content");
+        }
+    });
+
+    // load-more articles
+    $(".cards-articles").slice(0, 6).show();
+    $("#more-articles").on("click", function (e) {
+        e.preventDefault();
+        $(".cards-articles:hidden").slice(0, 6).slideDown();
+        if ($(".cards-articles:hidden").length == 0) {
+            $(".more-articles").text("").addClass("more-articles_no-content");
+        }
+    });
+
     // carousel homepage
     $('.about-team__slider').slick({
         dots: true,
@@ -56,5 +96,4 @@ $(document).on('click', '.advantages__button', function () {
     var show = $(this).data('show');
     $(show).removeClass("advantages__wrap-content_hide").siblings().addClass("advantages__wrap-content_hide");
 });
-
 
